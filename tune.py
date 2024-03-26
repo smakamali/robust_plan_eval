39467a261def981c49c9138a42301bd5b699f427
smakamali <sma.kamali@gmail.com>
2024-03-26 19:00:03 +0000

    add support for neo and bao

--- tune.py
+++ tune.py
@@ -11,17 +11,17 @@ from pyg_data import queryPlanPGDataset
 from util.util import set_seed
 from util.data_transform import *
 from lcm.roq_model import lcm_pl as roq
-# from lcm.neo_bao_model import lcm_pl as neo_bao
-from util.custom_loss import aleatoric_loss
+from lcm.neo_bao_model import lcm_pl as neo_bao
+from util.custom_loss import aleatoric_loss, rmse_loss
 
 # Experiment parameters
-architecture_p = 'roq'
+architecture_p = 'neo'
 experiment_id = 'job_syn'
-cpus_per_trial = 5 # determines the number of cpus used by each experiment. when OOM happens, reducing this value may help to resolve by reducing the number of concurrent 
-gpus_per_trial = 0.25 # determines the number (or ratio) of gpus used by each experiment
+cpus_per_trial = 3 # determines the number of cpus used by each experiment. when OOM happens, reducing this value may help to resolve by reducing the number of concurrent 
+gpus_per_trial = 0.14 # determines the number (or ratio) of gpus used by each experiment
 num_samples=500
 num_epochs=64
-patience=10
+patience=6
 
 # queryPlanPGDataset data module parameteres
 files_id = 'job_syn_all'
@@ -30,7 +30,7 @@ results_dir ='./param_data/'
 seed = 0
 reload_data = False
 val_ratio = .1
-test_ratio = .1
+test_ratio = 200
 
 if not os.path.exists(results_dir):
     os.mkdir(results_dir)
@@ -80,10 +80,10 @@ val_set = yTransFunc.transform(val_set)
 test_set = yTransFunc.transform(test_set)
 
 config = {
-    'node_embd_dim': tune.choice([8,16,32,64]),
-    'query_module_out': tune.choice([32,64,128,256]),
-    'qp_attheads': tune.randint(1,3),
-    'qp_gnn_ls':tune.randint(2,5),
+    # Neo's param search space
+
+    'query_module_in': tune.choice([128,256,512]),
+    'query_module_out': tune.choice([16,32,64]),
 
     'TCNNin':tune.choice([64,128,256]),
     'TCNNout':tune.choice([32,64]),
@@ -94,7 +94,53 @@ config = {
     'batch_size': tune.choice([128,256,512]),
     'dropout': tune.uniform(0.05,.2),
     'lr': tune.qloguniform(1e-4, 1e-2, 5e-5),
-    }
+}
+
+
+# {
+#     # Bao's param search space
+#     'TCNNin':tune.choice([64,128,256]),
+#     'TCNNout':tune.choice([32,64]),
+
+#     'finalMLPin': tune.choice([128,256,512]),
+#     'finalMLPout': tune.choice([64,128]),
+    
+#     'batch_size': tune.choice([128,256,512]),
+#     'dropout': tune.uniform(0.05,.2),
+#     'lr': tune.qloguniform(1e-4, 1e-2, 5e-5),
+#     }  
+
+    # Roq's param fine tune lr 
+    # 'node_embd_dim': tune.choice([32]),
+    # 'query_module_out': tune.choice([128]),
+    # 'qp_attheads': tune.choice([1]),
+    # 'qp_gnn_ls':tune.choice([2]),
+
+    # 'TCNNin':tune.choice([64]),
+    # 'TCNNout':tune.choice([32]),
+
+    # 'finalMLPin': tune.choice([128]),
+    # 'finalMLPout': tune.choice([64]),
+    
+    # 'batch_size': tune.choice([256]),
+    # 'dropout': tune.uniform(0.05,.2),
+    # 'lr': tune.qloguniform(1e-4, 1e-2, 5e-5),
+
+    # Roq's param search space
+    # 'node_embd_dim': tune.choice([8,16,32,64]),
+    # 'query_module_out': tune.choice([32,64,128,256]),
+    # 'qp_attheads': tune.randint(1,3),
+    # 'qp_gnn_ls':tune.randint(2,5),
+
+    # 'TCNNin':tune.choice([64,128,256]),
+    # 'TCNNout':tune.choice([32,64]),
+
+    # 'finalMLPin': tune.choice([128,256,512]),
+    # 'finalMLPout': tune.choice([64,128]),
+    
+    # 'batch_size': tune.choice([128,256,512]),
+    # 'dropout': tune.uniform(0.05,.2),
+    # 'lr': tune.qloguniform(1e-4, 1e-2, 5e-5),
 
 plan_attr_shape = train_set[0].plan_attr.shape
 plan_ord_shape = train_set[0].plan_ord.shape
@@ -103,7 +149,7 @@ edge_attr_shape = train_set[0].edge_attr_s.shape
 node_attr_shape = train_set[0].x_s.shape
 
 
-def train_model(config,num_epochs,data,cpus):
+def train_model(config,num_epochs,data,cpus,architecture_p):
     batch_size = config['batch_size']
     follow_batch = ['x_s']
     print('batch_size', batch_size)
@@ -123,19 +169,39 @@ def train_model(config,num_epochs,data,cpus):
     
     torch.set_float32_matmul_precision('medium')
 
-    criterion = aleatoric_loss(device=device)
+    if 'roq' in architecture_p:
+        
+        criterion = aleatoric_loss(device=device)
 
-    model = roq(
-        num_node = node_attr_shape[0], 
-        node_dim = node_attr_shape[1],
-        edge_dim = edge_attr_shape[1],
-        numPlanFeat=plan_attr_shape,
-        numPlanOrdFeat=plan_ord_shape,
-        numQueryGraphFeat = graph_attr_shape[0],
-        with_var = True, device = device,
-        criterion = criterion,
-        **config
-        )
+        model = roq(
+            num_node = node_attr_shape[0], 
+            node_dim = node_attr_shape[1],
+            edge_dim = edge_attr_shape[1],
+            numPlanFeat=plan_attr_shape,
+            numPlanOrdFeat=plan_ord_shape,
+            numQueryGraphFeat = graph_attr_shape[0],
+            with_var = True, device = device,
+            criterion = criterion,
+            **config
+            )
+    
+    elif 'neo' in architecture_p or 'bao' in architecture_p:
+        
+        criterion = rmse_loss()
+
+        model = neo_bao(
+            num_node = node_attr_shape[0], 
+            node_dim = node_attr_shape[1],
+            edge_dim = edge_attr_shape[1],
+            numPlanFeat=plan_attr_shape,
+            numPlanOrdFeat=plan_ord_shape,
+            numQueryGraphFeat = graph_attr_shape[0],
+            device = device, 
+            criterion = criterion,
+            architecture = architecture_p,
+            **config
+            )
+
     
     metrics = {"loss": "ptl/val_loss"}
     ray_cb = TuneReportCheckpointCallback(
@@ -157,7 +223,7 @@ def train_model(config,num_epochs,data,cpus):
     
     trainer.fit(model,train_loader,val_loader)
 
-def tune_lcm_gnn_pl(num_samples=1000, num_epochs=400, cpus = 2,gpus_per_trial = 0):
+def tune_lcm_gnn_pl(num_samples=1000, num_epochs=400, cpus = 2, gpus_per_trial = 0, architecture_p = 'roq'):
     
     train_ref = ray.put(train_set)
     val_ref = ray.put(val_set)    
@@ -173,7 +239,8 @@ def tune_lcm_gnn_pl(num_samples=1000, num_epochs=400, cpus = 2,gpus_per_trial = 
         train_model,
         num_epochs=num_epochs,
         data=data,
-        cpus=cpus
+        cpus=cpus,
+        architecture_p=architecture_p,
         # num_gpus=gpus_per_trial,
         # query_processor = 'gnn'
         # static_config=static_config,
@@ -204,7 +271,7 @@ def tune_lcm_gnn_pl(num_samples=1000, num_epochs=400, cpus = 2,gpus_per_trial = 
     print("Best hyperparameters found were: ", results.get_best_result().config)
     return results
 
-results = tune_lcm_gnn_pl(num_samples=num_samples, num_epochs=num_epochs, cpus = cpus_per_trial, gpus_per_trial = gpus_per_trial)
+results = tune_lcm_gnn_pl(num_samples=num_samples, num_epochs=num_epochs, cpus = cpus_per_trial, gpus_per_trial = gpus_per_trial, architecture_p = architecture_p)
 
 # save param tuning results to disk
 results_df = results.get_dataframe()
