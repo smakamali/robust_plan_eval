751209a22444cf96fc758edda3513e72ca2958ec
AminK <sma.kamali@gmail.com>
2025-03-08 01:37:18 +0000

    updating eval.py to evalaute lero and balsa too

--- eval.py
+++ eval.py
@@ -8,10 +8,12 @@ import pandas as pd
 from pyg_data import queryPlanPGDataset_withbenchmark
 from util.util import set_seed, load_model_params, load_best_model_paths
 from util.data_transform import *
-from util.custom_loss import aleatoric_loss, rmse_loss
+from util.custom_loss import aleatoric_loss, rmse_loss, bce_loss
 import pytorch_lightning as pl
 from lcm.roq_model import lcm_pl as roq
 from lcm.neo_bao_model import lcm_pl as neo_bao
+from lcm.lero_model import LeroModelPairwise as leroPairwise
+from lcm.balsa_model import balsa_model
 from torch_geometric.loader import DataLoader
 from util.torch_util import LitMCdropoutModel
 from util.eval_util import *
@@ -21,9 +23,11 @@ results_dir = os.path.join('.','results')
    
 def test(
         experiment_id = 'job',
-        files_id = 'temp', try_gpu_inf=True, 
+        file_experiment_id = None,
+        files_id = 'temp', proc_files_id=None, try_gpu_inf=True, 
         seed = 1, num_workers=5, show_fig = False,
         benchmark_files_id='',
+        labeled_data_dir = './labeled_data/',
         test_split = 'test',
         cons_ratio_sp = np.arange(0.0,3,0.1),
         prune_ratio_sp = np.arange(0,.75,0.05),
@@ -34,6 +38,9 @@ def test(
         ):
 
     set_seed(seed)
+
+    if file_experiment_id is None:
+        file_experiment_id = experiment_id
 
     # setting torch.device()
     if try_gpu_inf:
@@ -50,24 +57,43 @@ def test(
         os.mkdir(results_dir)
 
     # loading model hyper-parameters
-    roq_config = load_model_params('roq')
-    neo_config = load_model_params('neo')
-    bao_config = load_model_params('bao')
+    for benchmark in ['ceb','dsb','job']:
+        if benchmark in experiment_id:
+            config_file=f'model_params_{benchmark}.cfg'
+            break
+    
+    roq_config = load_model_params('roq',config_file=config_file)
+    neo_config = load_model_params('neo',config_file=config_file)
+    bao_config = load_model_params('bao',config_file=config_file)
+    lero_config = load_model_params('lero',config_file=config_file)
+    balsa_config = load_model_params('balsa',config_file=config_file)
+
+    # drop training hyper-parameters from the configs
+    configs = [roq_config,neo_config,bao_config,lero_config,balsa_config]
+    params_to_drop = ['max_epochs','patience','min_delta']
+    for config in configs:
+        for param in params_to_drop:
+            if param in config:
+                config.pop(param)
 
     # loading model paths
     best_model_paths = load_best_model_paths('roq',experiment_id)
     neo_paths =  load_best_model_paths('neo',experiment_id)
     bao_paths =  load_best_model_paths('bao',experiment_id)
+    lero_paths = load_best_model_paths('lero',experiment_id)
+    balsa_paths = load_best_model_paths('balsa',experiment_id)
 
     # Load train, validation, and test datasets
     print("loading train")
     train_set = queryPlanPGDataset_withbenchmark(
         split= 'train', files_id = files_id,
+        proc_files_id = proc_files_id,
         benchmark_files_id=benchmark_files_id,
+        labeled_data_dir=labeled_data_dir,  
         force_reload=reload_data, seed = seed,
         val_samples = val_samples, test_samples = test_samples,
         test_slow_samples = test_slow_samples,
-        exp_id=experiment_id
+        exp_id=file_experiment_id
         )
     print("{} queries and {} samples in training dataset: ".format(np.unique(np.array(train_set.query_id)).shape[0],train_set.len()))
     
@@ -75,7 +101,8 @@ def test(
     val_set = queryPlanPGDataset_withbenchmark(
         split= 'val', 
         files_id = files_id,
-        exp_id=experiment_id
+        proc_files_id=proc_files_id,
+        exp_id=file_experiment_id
         )
     print("{} queries and {} samples in vlidation dataset: ".format(np.unique(np.array(val_set.query_id)).shape[0],val_set.len()))
     
@@ -83,16 +110,12 @@ def test(
     test_set = queryPlanPGDataset_withbenchmark(
         split= test_split, 
         files_id = files_id,
-        exp_id=experiment_id
+        proc_files_id=proc_files_id,
+        exp_id=file_experiment_id
         )
     print("{} queries and {} samples in test dataset: ".format(np.unique(np.array(test_set.query_id)).shape[0],test_set.len()))
     
     # Perform data transformations on inputs 
-    drop_const = dropConst(train_set)
-    train_set = drop_const(train_set)
-    val_set = drop_const(val_set)
-    test_set = drop_const(test_set)
-
     null_imp = nullImputation(train_set)
     train_set = null_imp(train_set)
     val_set = null_imp(val_set)
@@ -142,6 +165,7 @@ def test(
         )
     
     aleatoric_l = aleatoric_loss(device=device)
+    bce_l = bce_loss()
     rmse_l = rmse_loss()
 
     torch.set_float32_matmul_precision('high')
@@ -153,14 +177,20 @@ def test(
     agg_res_dict = {}
 
     # For loop starts here
+    max_num_models = max(len(best_model_paths),len(neo_paths),len(bao_paths),len(lero_paths),len(balsa_paths))
+    for i in range(max_num_models):
+        # change seed for each iteration
+        seed+=1
+        set_seed(seed)
 
-    # for i,_ in enumerate(best_model_paths):
-    for i in [0,1,2,4]:
-        best_model_path = best_model_paths[i]
-        neo_path = neo_paths[i]
-        bao_path = bao_paths[i]
+        # get model paths
+        best_model_path = best_model_paths[i] if i < len(best_model_paths) else best_model_paths[-1]
+        neo_path = neo_paths[i] if i < len(neo_paths) else neo_paths[-1]
+        bao_path = bao_paths[i] if i < len(bao_paths) else bao_paths[-1]
+        lero_path = lero_paths[i] if i < len(lero_paths) else lero_paths[-1]
+        balsa_path = balsa_paths[i] if i < len(balsa_paths) else balsa_paths[-1]
 
-        # Note: model paramteres must be passed unless they are the same of defaults
+        # Note: model paramteres must be passed unless they are the same as defaults
         model = roq.load_from_checkpoint(
             best_model_path, 
             num_node = node_attr_shape[0], 
@@ -202,6 +232,34 @@ def test(
             **bao_config
             )
 
+        lero_pairwise = leroPairwise.load_from_checkpoint(
+            lero_path,
+            num_node = node_attr_shape[0], 
+            numPlanFeat=plan_attr_shape,
+            numPlanOrdFeat=plan_ord_shape,
+            numQueryGraphFeat = graph_attr_shape[0],
+            device = device, 
+            criterion = rmse_l,
+            **lero_config
+            )
+        lero=lero_pairwise.lero_model
+
+        pretrain_balsa = balsa_config.pop('pretrain',False)
+        balsa = balsa_model.load_from_checkpoint(
+            balsa_path,
+            num_node = node_attr_shape[0], 
+            node_dim = node_attr_shape[1],
+            edge_dim = edge_attr_shape[1],#fill_value =0, 
+            numPlanFeat=plan_attr_shape,
+            numPlanOrdFeat=plan_ord_shape,
+            numQueryGraphFeat = graph_attr_shape[0],
+            device = device, 
+            criterion = rmse_l,
+            **balsa_config
+            )
+
+        
+
         # Transform targets for Roq
         train_set = yTransFunc_log.transform(train_set)
         val_set = yTransFunc_log.transform(val_set)
@@ -238,23 +296,7 @@ def test(
         ypreds_test_m_org=yTransFunc_log.inverse_transform(torch.Tensor(ypreds_test_m))
 
         ########## Make predication for Neo and Bao ##########
-
-        # Replace the transformed targets with ones suitable for Neo and Bao
-        # train_set = yTransFunc.transform(train_set)
-        # val_set = yTransFunc.transform(val_set)
-        # test_set = yTransFunc.transform(test_set)
-
-        val_loader = DataLoader(
-            val_set, batch_size=batch_size,
-            shuffle=False, num_workers=num_workers, follow_batch=follow_batch,
-            #  persistent_workers=True
-            )
-        test_loader = DataLoader(
-            test_set, batch_size=batch_size,
-            shuffle=False, num_workers=num_workers, follow_batch=follow_batch,
-            #  persistent_workers=True
-            )
-        
+     
         # Model Inference
         # Neo
         ypreds_test_neo_vanilla = trainer.predict(neo_vanilla, test_loader)
@@ -266,6 +308,15 @@ def test(
         bao_preds2_vanilla = torch.vstack(ypreds_test_bao_vanilla).numpy()
         bao_preds2_org_vanilla=yTransFunc_log.inverse_transform(torch.Tensor(bao_preds2_vanilla))
 
+        # Lero
+        ypreds_test_lero = trainer.predict(lero, test_loader)
+        ypreds_test_lero = torch.vstack(ypreds_test_lero)
+        # ypreds_test_lero=yTransFunc.inverse_transform(torch.Tensor(ypreds_test_lero))
+
+        # Balsa
+        ypreds_test_balsa = trainer.predict(balsa, test_loader)
+        ypreds_test_balsa = torch.vstack(ypreds_test_balsa)
+        ypreds_test_balsa=yTransFunc_log.inverse_transform(torch.Tensor(ypreds_test_balsa))
         
         ############ Tuning strategy parameters ############
         
@@ -566,6 +617,13 @@ def test(
             bao_preds2_vanilla[:,0],bao_preds2_vanilla[:,1],test_set , 
             strategy='baseline ml')
 
+        lero_subopt,lero_runtime,_ = evaluate_method(
+            ypreds_test_lero[:,0],ypreds_test_lero[:,1],test_set, 
+            strategy='baseline ml')
+    
+        balsa_subopt,balsa_runtime,_ = evaluate_method(
+            ypreds_test_balsa[:,0],ypreds_test_balsa[:,1],test_set, 
+            strategy='baseline ml')
 
         ################## Compute q-error ##################
         q_error_num_joins, _ = lcm_model_eval(
@@ -592,13 +650,27 @@ def test(
             ,load_from_disk=False, files_id = files_id,show_fig=show_fig
             )
         
+        lero_qerror, _ = lcm_model_eval(
+            test_set, ypreds_test_lero[:,0], 
+            dataset_label = 'test_data', model_label = 'lero'
+            ,load_from_disk=False, files_id = files_id,show_fig=show_fig
+            )
+        
+        balsa_qerror, _ = lcm_model_eval(
+            test_set, ypreds_test_balsa[:,0], 
+            dataset_label = 'test_data', model_label = 'balsa'
+            ,load_from_disk=False, files_id = files_id,show_fig=show_fig
+            )
+        
         qerrors = pd.DataFrame(
             [bao_qerror2_vanilla.q_error,
              neo_qerror2_vanilla.q_error,
+            lero_qerror.q_error,
+            balsa_qerror.q_error,
             q_error_num_joins.q_error,
             roq_mcdp_num_joins.q_error
             ]).T
-        qerrors.columns = ['Bao', 'Neo','Roq','Roq MCDO']
+        qerrors.columns = ['Bao','Neo','Lero','Balsa','Roq','Roq MCDO']
     
 
         ########### Compute Perason's correlations ############
@@ -608,6 +680,8 @@ def test(
         pearson_coef5, _ = compute_pearsonr(bao_preds2_org_vanilla[:,0].squeeze(),target)
         pearson_coef6, _ = compute_pearsonr(neo_preds2_org_vanilla[:,0].squeeze(),target)
         pearson_coef7, _ = compute_pearsonr(ypreds_test_m_org,target)
+        pearson_coef8, _ = compute_pearsonr(ypreds_test_lero[:,0].squeeze(),target)
+        pearson_coef9, _ = compute_pearsonr(ypreds_test_balsa[:,0].squeeze(),target)
 
         ########### Compute Spearman's correlations ###########
         spearmanr_coef1, _ = compute_spearmanr((test_set.opt_cost),target)
@@ -615,28 +689,29 @@ def test(
         spearmanr_coef5, _ = compute_spearmanr(bao_preds2_org_vanilla[:,0].squeeze(),target)
         spearmanr_coef6, _ = compute_spearmanr(neo_preds2_org_vanilla[:,0].squeeze(),target)
         spearmanr_coef7, _ = compute_spearmanr(ypreds_test_m_org,target)
+        spearmanr_coef8, _ = compute_spearmanr(ypreds_test_lero[:,0].squeeze(),target)
+        spearmanr_coef9, _ = compute_spearmanr(ypreds_test_balsa[:,0].squeeze(),target)
 
-        labels = ['Cost','Bao', 'Neo','Roq','Roq MCDO']
-        
         pearson_values = [
             pearson_coef1,pearson_coef5,pearson_coef6,
-            pearson_coef4,pearson_coef7
+            pearson_coef8,pearson_coef9,pearson_coef4,pearson_coef7
             ]
         
         spearman_values = [
             spearmanr_coef1,spearmanr_coef5,spearmanr_coef6,
-            spearmanr_coef4,spearmanr_coef7
+            spearmanr_coef8,spearmanr_coef9,spearmanr_coef4,spearmanr_coef7
             ]
         
+        model_labels = ['Cost','Bao','Neo','Lero','Balsa','Roq','Roq MCDO']
         corr_df = pd.DataFrame(
             np.array([pearson_values,spearman_values]), 
-            columns=labels,index=['Pearson','Spearman']
+            columns=model_labels,index=['Pearson','Spearman']
             )
 
         labels = [
             'optimizer',
             'optimizer pruned',
-            'Neo','Bao',
+            'Neo','Bao', 'Lero', 'Balsa',
             'base model',
             'base model pruned',
             'mc dropout',
@@ -666,6 +741,7 @@ def test(
             test_opt_runtimes,
             test_opt_prune_runtimes,
             neo_runtime2_vanilla,bao_runtime2_vanilla,
+            lero_runtime, balsa_runtime,
             test_base_ml_runtimes,
             test_base_ml_prune_runtimes,
             test_mc_ml_runtimes,
@@ -695,6 +771,7 @@ def test(
             test_opt_subopts,
             test_opt_prune_subopts,
             neo_subopt2_vanilla,bao_subopt2_vanilla,
+            lero_subopt, balsa_subopt,
             test_base_ml_subopts,
             test_base_ml_prune_subopts,
             test_mc_ml_subopts,
@@ -767,18 +844,21 @@ def test(
 if __name__ == '__main__':
 
     test(
-        experiment_id = 'ceb_1000_x5_s3_loss',
+        experiment_id = 'ceb_1000_x5_s317',
+        file_experiment_id = 'ceb_1000',
         files_id = 'ceb_1000',
-        benchmark_files_id = 'job_main',
-        test_split = 'benchmark',
+        proc_files_id='ceb_1000',
+        benchmark_files_id = 'job_v2.2',
+        labeled_data_dir='./labeled_data/ceb/',
+        test_split = 'test',
         try_gpu_inf=True,
         num_workers=1,
         show_fig = False,
-        cons_ratio_sp = np.arange(0.0,3,0.05),
-        prune_ratio_sp = np.arange(0,.75,0.05),
-        seed = 3,
+        cons_ratio_sp = np.arange(0.0,3,0.01),
+        prune_ratio_sp = np.arange(0,.3,0.025),
+        seed = 310,
         reload_data = False,
         val_samples = 0.1,
-        test_samples = 100,
-        test_slow_samples = None
+        test_samples = 0.1,
+        test_slow_samples = 0.5
         )
